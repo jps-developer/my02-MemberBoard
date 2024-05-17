3062703be59c00e56a275f9de4923cc466b2e66c
jaepil <jaepill94@gmail.com>
2024-05-17 14:53:00 +0900

    Feat: Jpa MemberRepository add Jpa 기술로 회원저장소 추가 Related to: #22

--- src/main/java/myproject/memberboard/MemberboardApplication.java
+++ src/main/java/myproject/memberboard/MemberboardApplication.java
@@ -1,10 +1,7 @@
 package myproject.memberboard;
 
 import myproject.memberboard.domain.board.repository.BoardRepository;
-import myproject.memberboard.domain.config.JDBCTemplateConfig;
-import myproject.memberboard.domain.config.MemoryRepositoryConfig;
-import myproject.memberboard.domain.config.MybatisConfig;
-import myproject.memberboard.domain.config.SQLExceptionTranslatorConfig;
+import myproject.memberboard.domain.config.*;
 import myproject.memberboard.domain.member.repository.JDBCMemberRepository;
 import myproject.memberboard.domain.member.repository.MemberRepository;
 import myproject.memberboard.domain.member.repository.SQLExceptionTranslatorMemberRepository;
@@ -18,7 +15,8 @@ import org.springframework.context.annotation.Profile;
 //@Import(MemoryRepositoryConfig.class)
 //@Import(SQLExceptionTranslatorConfig.class)
 //@Import(JDBCTemplateConfig.class)
-@Import(MybatisConfig.class)
+//@Import(MybatisConfig.class)
+@Import(JpaConfig.class)
 @SpringBootApplication(scanBasePackages = "myproject.memberboard.web")
 public class MemberboardApplication {
 
